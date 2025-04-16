72adc5ddbc7cac9f6c6af904a62b49beb0e6921f
1165qwc <160745739+1165qwc@users.noreply.github.com>
2025-04-16 11:02:31 +0800

    Update PDFChat.py

--- PDFChat.py
+++ PDFChat.py
@@ -8,17 +8,15 @@ upload_folder = 'uploaded_pdf_file'
 if not os.path.exists(upload_folder):
     os.mkdir(upload_folder)
 
-st.title("PDF Chatbot")
+st.header("PDF Chatbot")
 
 uploaded_file = st.file_uploader("Choose a pdf file", type=['pdf','PDF'])
 
-text = ""  # Initialize text variable
-
 if uploaded_file is not None:
     file_name = uploaded_file.name
-    saved_path = os.path.join(upload_folder, file_name)
+    saved_path = os.path.join(upload_folder,file_name)
 
-    with open(saved_path, 'wb') as f:
+    with open(saved_path,'wb') as f:
         f.write(uploaded_file.getbuffer())
 
     st.success(f"PDF file has successfully uploaded to {saved_path}")
@@ -26,30 +24,27 @@ if uploaded_file is not None:
     reader = PdfReader(saved_path)
     number_of_pages = len(reader.pages)
     page = reader.pages[0]
-    text = page.extract_text()  # Extract text from the first page
+    text = page.extract_text()
 
-    st.write(text)
+    st.write(text)    
 
 def response_generator(text,prompt):
-    # API_URL = "https://router.huggingface.co/hf-inference/models/google-bert/bert-large-uncased-whole-word-masking-finetuned-squad"
-    # headers = {"Authorization": "Bearer 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"}
-
-    API_URL = "http://127.0.0.1:11434/api/text"
+    API_URL = "https://router.huggingface.co/hf-inference/models/google-bert/bert-large-uncased-whole-word-masking-finetuned-squad"
+    headers = {"Authorization": "Bearer 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"}
 
     payload = ({
-        "model" : "deepseek-r1:1.5b",
     "inputs": {
         "question": prompt,
         "context": text
     },
     })
 
-    response = requests.post(API_URL, json=payload, stream=True)
+    response = requests.post(API_URL, headers=headers, json=payload)
     output = response.json()
 
     return output
 
-st.header("Chat")
+st.title("Simple Chat")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -62,12 +57,8 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
+    response = response_generator(text,prompt)
+    with st.chat_message("assistant"):
+        st.markdown(response['answer'])
+    st.session_state.messages.append({"role": "assistant", "content": response['answer']})
     
-    # Ensure text is not empty before calling response_generator
-    if text:
-        response = response_generator(text, prompt)
-        with st.chat_message("assistant"):
-            st.markdown(response['answer'])
-        st.session_state.messages.append({"role": "assistant", "content": response['answer']})
-    else:
-        st.error("No text extracted from the PDF.")
